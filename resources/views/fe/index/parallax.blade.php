6df7460852c0365d5621129d96791ab05a9cbd8f
melanie200422 <116139837+melanie200422@users.noreply.github.com>
2023-07-04 14:33:55 +0700

    xcscvs

--- resources/views/fe/index/parallax.blade.php
+++ resources/views/fe/index/parallax.blade.php
@@ -4,8 +4,8 @@
                 <div class="hero__inner">
                     <div class="container">
                         <div class="wrap-text left text-small font-bold">
-                            <h2 class="h2 mega-title">Belle <br> The best choice for your store</h2>
-                            <div class="rte-setting mega-subtitle">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</div>
+                            <h2 class="h2 mega-title">Fendi Perfume <br> The best choice for your store</h2>
+                            <div class="rte-setting mega-subtitle">As sexy as a cotton mini dress and as elegant as the cocktails at Mama Shelter, leaving you overwhelmed and happy.</div>
                             <a href="#" class="btn">Purchase Now</a>
                         </div>
                     </div>
