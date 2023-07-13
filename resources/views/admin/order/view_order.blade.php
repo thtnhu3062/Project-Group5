ee0a8389194aa72d4b498b2f9662c9735cc63834
Elnino090292 <114639587+Elnino090292@users.noreply.github.com>
2023-07-13 12:23:02 +0700

    up
    up

--- resources/views/admin/order/view_order.blade.php
+++ resources/views/admin/order/view_order.blade.php
@@ -5,33 +5,6 @@
    <div class="container-fluid">
       <div class="row">
          <div class="col-sm-12">
-            <div class="iq-card">
-               <div class="iq-card-header d-flex justify-content-between">
-                  <div class="iq-header-title">
-                     <h4 class="card-title">Customer's information</h4>
-                  </div>
-               </div>
-               <div class="iq-card-body">
-                  <div id="table" class="table-editable">
-                     <table class="table table-bordered table-responsive-md table-striped text-center">
-                        <thead>
-                           <tr>
-                              <th>Name</th>
-                              <th>Email</th>
-                              <th>Phone</th>
-                           </tr>
-                        </thead>
-                        <tbody>
-                           <tr>
-                              <td>{{$customer->customer_name}}</td>
-                              <td>{{$customer->customer_email}}</td>
-                              <td>{{$customer->customer_phone}}</td>
-                           </tr>
-                        </tbody>
-                     </table>
-                  </div>
-               </div>
-            </div>
          </div>
       </div>
    </div>
@@ -62,7 +35,7 @@
                               <td>{{$shipping->shipping_name}}</td>
                               <td>{{$shipping->shipping_address}}</td>
                               <td>{{$shipping->shipping_phone}}</td>
-                              <td>{{$shipping->shipping_phone}}</td>
+                              <td>{{$shipping->shipping_note}}</td>
                            </tr>
 
                         </tbody>
